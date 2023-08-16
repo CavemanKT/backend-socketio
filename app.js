474cd056ba0a343cf41072765089b058f3e2d254
kt <kt@skilledup.life>
2023-08-16 22:05:19 +0800

    fixed the data object , username, but leave 3 mark on top of the chat room

--- app.js
+++ app.js
@@ -6,15 +6,17 @@ const io = require("socket.io")(8000, {
 
 io.on("connection", socket => {
     // console.log(socket.id);
-    socket.on('message', (data, room) => {
-        if (room == '') {
+    socket.on('message', data => {
+        console.log(data.username);
+
+        if (data.room == '') {
             socket.broadcast.emit('receive-message', data)
 
         } else {
-            socket.to(room).emit('receive-message', data)
+            socket.to(data.room).emit('receive-message', data)
             console.log("send to room with message: ", data);
         }
-        console.log(data, room)
+        console.log(data)
     })
     socket.on('join-room', (room, cb) => {
         socket.join(room.room)
