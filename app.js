8c990a0aa1fba77a2496d62c7095b6837df75a95
kt <kt@skilledup.life>
2023-08-17 15:23:30 +0800

    remaining: joinRoom, leaveRoom, users, rooms, how to disconnect.. and how to make hooks and tidier code and file structure.

--- app.js
+++ app.js
@@ -4,23 +4,24 @@ const io = require("socket.io")(8000, {
     }
 })
 
+
+let users = []
 io.on("connection", socket => {
-    // console.log(socket.id);
     socket.on('message', data => {
         console.log(data.username);
-
         if (data.room == '') {
             socket.broadcast.emit('receive-message', data)
-
-        } else {
+        } else {  // only this case for now because you already join room by the time you need to speak
             socket.to(data.room).emit('receive-message', data)
             console.log("send to room with message: ", data);
         }
         console.log(data)
     })
-    socket.on('join-room', (room, cb) => {
-        socket.join(room.room)
-        console.log("backend-line join-room 21:", room.room);
-        cb(`Welcome ${room.username}to the discussion - ${room.room}`)
+    socket.on('join-room', (data, cb) => {
+        socket.join(data.room)
+        users.push(data)
+        console.log("backend-line join-room 21:", data.room);
+        cb(`Welcome ${data.username} to the discussion - ${data.room}`)
+        socket.to(data.room).emit("broadcast-who-joins", data)
     })
 })
